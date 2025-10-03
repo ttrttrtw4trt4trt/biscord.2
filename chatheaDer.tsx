d9203285d82f8c0aa2a8db62067c3023b4cb835d
thegoatman367 <8972923958@student.cms.k12.nc.us>
2025-10-03 09:12:10 -0400

    chatheaDer.tsx

---
+++ chatheaDer.tsx
@@ -0,0 +1,10 @@
+import ChatHeader from "../ChatHeader";
+
+export default function ChatHeaderExample() {
+  return (
+    <ChatHeader
+      channelName="general"
+      channelDescription="General discussion for the class"
+    />
+  );
+}
